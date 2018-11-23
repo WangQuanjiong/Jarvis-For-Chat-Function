97eb453c7d89d32b8751c5c82ab2eff0c40fb62f
WangQJ <wangquanjiong@outlook.com>
2018-11-24 01:45:04 +0800

    - 重写主窗layout - 增加头像

--- pyqt_learning_by_Wangquanjiong/Main-Window.py
+++ pyqt_learning_by_Wangquanjiong/Main-Window.py
@@ -3,7 +3,8 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
-
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
 class Main_Window(QWidget):
     def __init__(self):
 #--------------------------主窗体--------------------------------------#        
@@ -12,51 +13,73 @@ class Main_Window(QWidget):
         self.resize(800,600)# 设置窗口初始位置和大小
         self.center()# 设置窗口居中       
         self.setWindowTitle('Jarvis For Chat 主界面框架')# 设置窗口标题
-        #self.setWindowFlag(Qt.FramelessWindowHint)
+        #主窗网格布局，基底
+        self.main_layout = QGridLayout(self,spacing=0)
         
-         # 水平布局，添加部件到布局中
-        self.main_layout = QHBoxLayout(self,spacing=0)
         self.main_layout.setContentsMargins(0,0,0,0)
         self.setLayout(self.main_layout)
-#------------------------左边菜单栏--------------------------------------#
+    #------------------------左边菜单栏--------------------------------------#
         self.LeftTabWidget = QListWidget()        
         self.LeftTabWidget.setFixedWidth(180)
-        #self.LeftTabWidget.setGeometry(0,40,180,1080)
         self.LeftTabWidget.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.LeftTabWidget.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.LeftTabWidget.setFrameShape(QListWidget.NoFrame)
-        self.main_layout.addWidget(self.LeftTabWidget)
-
+        self.LeftTabWidget.setFrameShape(QListWidget.NoFrame)        
+        #左侧组件设置
+        self.left_layout = QVBoxLayout()    
+        self.LeftTabWidget.setLayout(self.left_layout) #将左侧布局（垂直）绑定到左侧组件上
+        #左侧布局
+        self.main_layout.addWidget(self.LeftTabWidget,2,0)
+        
+        self.LeftTabWidget.currentRowChanged.connect(self.display) #绑定
         list_str = ['热点分析','关键词提醒','群发助手','单项好友删除']
         for i in range(4):
             self.item = QListWidgetItem(list_str[i],self.LeftTabWidget)   #左侧选项的添加
             self.item.setSizeHint(QSize(180,80))
+            #self.item.set(0,(i+1)*180)
             self.item.setTextAlignment(Qt.AlignCenter)                  #居中显示
-#--------------------------右边页面-------------------------------------------#       
+        
+    #--------------------左侧菜单栏头像------------------------------------------#
+        self.avatar_layout = QGridLayout()
+        self.main_layout.addLayout(self.avatar_layout,0,0)
+
+        self.avatar = QtWidgets.QPushButton()
+        self.avatar.setFixedSize(QSize(90,90))
+        self.avatar.setIcon(QIcon(r'D:\Jarvis-For-Chat\Resource\images\avatar1.png'))
+        self.avatar.setIconSize(QSize(90,90))
+        self.avatar_layout.addWidget(self.avatar,0,0,3,3)
+        #self.avatar_layout.setAlignment(self.avatar_layout,Qt.AlignRight)
+
+        self.weixin = QtWidgets.QPushButton()
+        self.weixin.setFixedSize(QSize(45,45))
+        self.weixin.setIcon(QIcon(r'D:\Jarvis-For-Chat\Resource\images\weixin1.png'))
+        self.avatar_layout.addWidget(self.weixin,2,4,1,1)
+        self.avatar_layout.setAlignment(self.weixin,Qt.AlignRight)
+        self.avatar_layout.setAlignment(self.weixin,Qt.AlignBottom)
+               
+    #--------------------------右边页面-------------------------------------------#       
+        # 在QStackedWidget对象中填充了4个子控件
+        self.stack = QStackedWidget(self)
+        self.right_layout = QGridLayout()
+        #self.main_layout.addLayout(self.right_layout,0,5)
+        self.stack.setLayout(self.right_layout)
+        self.main_layout.addWidget(self.stack,0,1,14,14)
+        #self.stack.setMinimumSize(620,600)
+        
         # 创建4个小控件和函数
         self.stack1 = QWidget()
         self.stack2 = QWidget()
         self.stack3 = QWidget()
         self.stack4 = QWidget()
-        #self.stack1.setFrameShape(QListWidget.NoFrame)
-        #self.stack2.setFrameShape(QListWidget.NoFrame)
-        #self.stack3.setFrameShape(QListWidget.NoFrame)
-        #self.stack4.setFrameShape(QListWidget.NoFrame)
+       
         self.stack1UI()
         self.stack2UI()
         self.stack3UI()
         self.stack4UI()
 
-        # 在QStackedWidget对象中填充了4个子控件
-        self.stack = QStackedWidget(self)
-        self.main_layout.addWidget(self.stack)
-        #self.stack.setMinimumSize(620,600)
         self.stack.addWidget(self.stack1)
         self.stack.addWidget(self.stack2)
         self.stack.addWidget(self.stack3)
         self.stack.addWidget(self.stack4)
-
-        self.LeftTabWidget.currentRowChanged.connect(self.display) #绑定
 
     def center(self):# 设置窗口居中
         self.qr = self.frameGeometry()
@@ -67,45 +90,18 @@ class Main_Window(QWidget):
     #热词分析
     def stack1UI(self):
         # 水平布局
-        self.layout=QFormLayout()
+        self.layout=QGridLayout()
         self.layout.setContentsMargins(0,0,0,0)
-        self.logo= QPixmap('E:/pyprogram/1120-界面切换的研究/image/LOGO.jpg')
+        
         self.l1=QLabel(self)
-        self.l1.setGeometry(0, 0, 150, 150)
-        #lbl.setScaledContents (True)  # 让图片自适应label大小
-        #self.l1.setStyleSheet("border: 2px solid blue")
-        self.l1.setPixmap(self.logo)
         self.l2 = QLabel('请在右侧列表中选择你感兴趣的群组及时间段', self)
         self.l3 = QLabel('开启你的【热词分析】之旅~', self)
         self.layout.addWidget(self.l1)
         self.layout.addWidget(self.l2)
         self.layout.addWidget(self.l3)
-
         self.qunlist = QListWidget()
-
         self.layout.addWidget(self.qunlist)
-        #lb2 = QLabel(self)
-        #lb2.setGeometry(0, 250, 300, 200)
-        #lb2.setPixmap(pix)
-        #lb2.setStyleSheet("border: 2px solid red")
-        #lb2.setScaledContents(True)
-
-        # 在窗口w中，新建一个lable，名字叫做l1
-        #self.l1=layout.addWidget(QLabel('x'))
-        #self.logo = QPixmap('E:/pyprogram/1120-界面切换的研究/image/LOGO.jpg')
-        # 在l1里面，调用setPixmap命令，建立一个图像存放框，并将之前的图像png存放在这个框框里。
-        #self.l1.setPix(self.logo)
-
-        # 在窗口w中，新建一个lable，名字叫做l2
-        #self.l2 = layout.addWidget(QLabel(self))
-        #self.file = open('E:/pyprogram/1120-界面切换的研究/文本/热词介绍P1.txt')
-        #self.reci=self.file.read()
-        #self.l2.setText(self.reci)
-
-        # 调整l1和l2的位置
-        #self.l1.move(100, 20)
-        #self.l2.move(140, 120)
-
+        
         self.stack1.setLayout(self.layout)
 
     #关键词提醒
@@ -121,7 +117,7 @@ class Main_Window(QWidget):
         #btn.setFrameShape
         #buntton.setGeometry(0,300,100,40)
         #buntton.setStyleSheet('backgroud:rgb(33,33,33)'；)
-        buntton.setStyleSheet('background: rgb(224，16，115);')
+        buntton.setStyleSheet('background: rgb(19,60,85);')
         buntton.setMinimumSize(620,600)
         main_layout.addWidget(buntton)
         
@@ -154,8 +150,8 @@ class Main_Window(QWidget):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    #style = open(r"D:\2018software\QssUI\StyleSheets\MetroUI.qss","r",encoding='utf-8')
-    #style_str = style.read()
+    style = open(r"D:\2018software\QssUI\StyleSheets\MetroUI.qss","r",encoding='utf-8')
+    style_str = style.read()
     #app.setStyleSheet(style_str)
 
     demo = Main_Window()
